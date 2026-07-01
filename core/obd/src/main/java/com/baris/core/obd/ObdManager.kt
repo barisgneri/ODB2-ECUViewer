86912055105f0304875db9e19a084aab1a2178c3
barisgneri <barisgnr.tr@gmail.com>
2026-07-01 19:15:58 +0300

    update pattern

---
+++ core/obd/src/main/java/com/baris/core/obd/ObdManager.kt
@@ -0,0 +1,13 @@
+package com.baris.core.obd
+
+import com.baris.core.model.ObdDataType
+import kotlinx.coroutines.flow.StateFlow
+
+interface ObdManager {
+    val liveData: StateFlow<Map<ObdDataType, Float>>
+    val isConnected: StateFlow<Boolean>
+
+    suspend fun connect(): Boolean
+    suspend fun startReading(dataTypes: Set<ObdDataType>)
+    suspend fun stopReading()
+}
